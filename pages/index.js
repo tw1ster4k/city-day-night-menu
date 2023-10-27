ccd2eae5955c396114b2bd2cc684a3a3801b89ed
tw1ster4k <macbookpro@MacBook-Pro-Macbook.local>
2023-10-27 06:17:44 +0200

    Release2.0.0

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Image from "next/image"
 import Logo from "../img/Logo.svg"
 import BurgerMenuIcon from "../img/BurgerMenuIcon.svg"
 import SearchIcon from "../img/SearchIcon.svg"
+import Link from "next/link"
 
 const index = () => {
+  const classNavScrollerItem = styles.navScrollerItem + " " + styles.navScrollerItemActive
   return (
     <div className={styles.main}>
         <header className={styles.header}>
@@ -18,6 +20,20 @@ const index = () => {
                 <div className={styles.search}>
                   <input className={styles.input} placeholder="Поиск по меню" />
                   <Image src={SearchIcon} className={styles.searchIcon} />
+                </div>
+                <div className={styles.navScroller}>
+                  <nav className={styles.navScrollerItems}>
+                      <Link className={classNavScrollerItem} href={"/"}>Коктейли</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Напитки</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Закуски</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Салаты</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Супы</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Пасты</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Мясо/Рыба</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Бургеры</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Гарниры</Link>
+                      <Link className={classNavScrollerItem} href={"/"}>Десерты</Link>  
+                  </nav>
                 </div>
               </div>
     </div>
